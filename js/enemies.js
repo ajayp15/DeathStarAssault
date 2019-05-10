cb1783cf81a3604f6b0146c7a0962d1667d7c85e
Antony Toron <atoron@princeton.edu>
2019-05-10 03:52:37 -0400

    Added explosion effects to exploding enemies

--- js/enemies.js
+++ js/enemies.js
@@ -11,7 +11,12 @@ function Enemy(scene) {
     this.mesh = createEnemy()
     this.shots = []
 
-    this.checkIfCollided = function(shape) {
+    // explosion stuff
+    this.dirs = undefined
+    this.explosionParticles = undefined
+    this.explosionIterations = 0 // how many times this explosion has been updated
+
+    this.checkIfCollided = function (shape) {
         var boundingShape = new THREE.Box3().setFromObject(shape)
 
         // first check if it hit the cockpit
@@ -51,7 +56,7 @@ function Enemy(scene) {
         return false
     }
 
-    this.shootAtTarget = function(target) {
+    this.shootAtTarget = function (target) {
         let plasmaBall = new THREE.Mesh(new THREE.SphereGeometry(0.1, 8, 4), new THREE.MeshBasicMaterial({
             color: "aqua"
         }));
@@ -60,29 +65,78 @@ function Enemy(scene) {
         var direction = new THREE.Vector3().subVectors(target.mesh.position, this.mesh.position).normalize()
         this.shots.push([plasmaBall, direction]);
     }
-    
-    this.handleLaserMovements = function(delta) {
+
+    this.handleLaserMovements = function (delta) {
         for (var i = 0; i < this.shots.length; i++) {
             var dir = this.shots[i][1]
             this.shots[i][0].translateOnAxis(dir.clone(), 5 * delta)
+        }
+    }
+
+    this.explode = function () {
+        var geometry = new THREE.Geometry();
+        var objectSize = 0.03
+        var movementSpeed = 5
+        this.dirs = []
+        var numParticles = 100
+        for (i = 0; i < numParticles; i++) {
+            var vertex = this.mesh.position.clone()
+
+            geometry.vertices.push(vertex);
+            this.dirs.push({ x: (Math.random() * movementSpeed) - (movementSpeed / 2), y: (Math.random() * movementSpeed) - (movementSpeed / 2), z: (Math.random() * movementSpeed) - (movementSpeed / 2) });
+        }
+        // could also do random-ish colors here
+        var material = new THREE.PointsMaterial({ size: objectSize, color: 0xf4bc42 });
+        var particles = new THREE.Points(geometry, material);
+
+        this.explosionParticles = particles
+        this.explosionParticles.geometry.verticesNeedUpdate = true;
+
+        this.scene.addMesh(particles);
+        // this.currentExplosionParticles = particles
+    }
+
+    this.updateExplosion = function(delta) {
+        var numParticles = 100
+        if (this.explosionParticles != undefined) {
+            for (var i = 0; i < numParticles; i++) {
+                var particle = this.explosionParticles.geometry.vertices[i]
+                particle.y += this.dirs[i].y * delta;
+                particle.x += this.dirs[i].x * delta;
+                particle.z += this.dirs[i].z * delta;
+                this.explosionParticles.geometry.vertices[i] = particle
+            }
+            this.explosionParticles.geometry.verticesNeedUpdate = true;
+            this.explosionIterations += 1
+        }
+        // stop it after some arbitrary time, don't want to render those particles
+        // forever
+        var seconds = 4
+        if (this.explosionParticles != undefined && 
+            this.explosionIterations >= 60 * seconds) { // assume called every 1/60th second
+            console.log("removing explosion")
+            this.scene.removeMesh(this.explosionParticles);
+            this.explosionParticles = undefined;
+            this.dirs = undefined
+            this.explosionIterations = 0
         }
     }
 }
 
 function createEnemy() {
     var sphereGeometry = new THREE.SphereGeometry(fighterRadius, 8, 5) // make it as low resolution as possible, to optimize
-    var bodyMaterial = new THREE.MeshLambertMaterial({color: 0x393f49, side : THREE.DoubleSide})
+    var bodyMaterial = new THREE.MeshLambertMaterial({ color: 0x393f49, side: THREE.DoubleSide })
     var body = new THREE.Mesh(sphereGeometry, bodyMaterial);
 
     // create wings now (just as circles) (6 segments, just like in real tie fighter)
     var circleGeometry1 = new THREE.CircleGeometry(wingRadius, 6)
-    var wingMaterial1 = new THREE.MeshLambertMaterial({color: 0x151616, side : THREE.DoubleSide})
+    var wingMaterial1 = new THREE.MeshLambertMaterial({ color: 0x151616, side: THREE.DoubleSide })
     var wing1 = new THREE.Mesh(circleGeometry1, wingMaterial1)
     wing1.position.x = -fighterRadius
     wing1.rotation.y = Math.PI / 2.1
 
     var circleGeometry2 = new THREE.CircleGeometry(wingRadius, 6)
-    var wingMaterial2 = new THREE.MeshLambertMaterial({color: 0x151616, side : THREE.DoubleSide})
+    var wingMaterial2 = new THREE.MeshLambertMaterial({ color: 0x151616, side: THREE.DoubleSide })
     var wing2 = new THREE.Mesh(circleGeometry2, wingMaterial2)
     wing2.position.x = fighterRadius
     wing2.rotation.y = Math.PI / 1.9
@@ -92,24 +146,30 @@ function createEnemy() {
         to look cool
     */
     var sphereGeometryWire = new THREE.SphereGeometry(fighterRadius, 8, 5) // make it as low resolution as possible, to optimize
-    var bodyMaterialWire = new THREE.MeshLambertMaterial({color: 0x27292d, 
-        side : THREE.DoubleSide,
-        wireframe: true})
+    var bodyMaterialWire = new THREE.MeshLambertMaterial({
+        color: 0x27292d,
+        side: THREE.DoubleSide,
+        wireframe: true
+    })
     var bodyWire = new THREE.Mesh(sphereGeometryWire, bodyMaterialWire);
 
     // create wings now (just as circles) (6 segments, just like in real tie fighter)
     var circleGeometry1Wire = new THREE.CircleGeometry(wingRadius, 6)
-    var wingMaterial1Wire = new THREE.MeshLambertMaterial({color: 0x393f49, 
-        side : THREE.DoubleSide,
-        wireframe: true})
+    var wingMaterial1Wire = new THREE.MeshLambertMaterial({
+        color: 0x393f49,
+        side: THREE.DoubleSide,
+        wireframe: true
+    })
     var wing1Wire = new THREE.Mesh(circleGeometry1Wire, wingMaterial1Wire)
     wing1Wire.position.x = -fighterRadius
     wing1Wire.rotation.y = Math.PI / 2.1
 
     var circleGeometry2Wire = new THREE.CircleGeometry(wingRadius, 6)
-    var wingMaterial2Wire = new THREE.MeshLambertMaterial({color: 0x393f49, 
-        side : THREE.DoubleSide,
-        wireframe: true})
+    var wingMaterial2Wire = new THREE.MeshLambertMaterial({
+        color: 0x393f49,
+        side: THREE.DoubleSide,
+        wireframe: true
+    })
     var wing2Wire = new THREE.Mesh(circleGeometry2Wire, wingMaterial2Wire)
     wing2Wire.position.x = fighterRadius
     wing2Wire.rotation.y = Math.PI / 1.9
@@ -118,7 +178,7 @@ function createEnemy() {
         Add the cockpit circle as a final touch
     */
     var sphereGeometry1 = new THREE.SphereGeometry(fighterRadius / 1.5, 8, 5) // make it as low resolution as possible, to optimize
-    var cockpitMaterial = new THREE.MeshLambertMaterial({color: 0x151616, side : THREE.DoubleSide})
+    var cockpitMaterial = new THREE.MeshLambertMaterial({ color: 0x151616, side: THREE.DoubleSide })
     var cockpit = new THREE.Mesh(sphereGeometry1, cockpitMaterial);
     cockpit.position.z = fighterRadius / 1.5
 
@@ -142,11 +202,11 @@ function Enemies(scene, plane) {
     this.plane = plane
     this.enemies = createEnemies()
 
-    this.handleEnemyMovements = function(delta) {
+    this.handleEnemyMovements = function (delta) {
         for (var i = 0; i < this.enemies.length; i++) {
             this.enemies[i].mesh.position.z += wallMovementSpeed * 2 * delta
 
-            this.enemies[i].mesh.rotation.z += delta * 0.1 *  Math.PI
+            this.enemies[i].mesh.rotation.z += delta * 0.1 * Math.PI
 
             // reposition them at far plane if they went out of view
             if (this.enemies[i].mesh.position.z > nearPlane) {
@@ -161,17 +221,20 @@ function Enemies(scene, plane) {
                 hasCollided = true
                 console.log("hit")
             }
+
+            // update explosions while here
+            this.enemies[i].updateExplosion(delta)
         }
     }
 
-    this.handleLaserMovements = function(delta) {
+    this.handleLaserMovements = function (delta) {
         for (var i = 0; i < this.enemies.length; i++) {
             this.enemies[i].handleLaserMovements(delta)
         }
     }
 
     // when being shot at
-    this.handleLaserCollisions = function() {
+    this.handleLaserCollisions = function () {
         var shotsFromPlane = this.plane.shots
         var shotsMissed = [] // return the shots that didn't hit anything, set plane shots buffer to that
         for (var i = 0; i < shotsFromPlane.length; i++) {
@@ -184,6 +247,7 @@ function Enemies(scene, plane) {
                     // (but don't actually remove it from the scene, just "explode" it
                     // and move it back to the far plane --> is this going to cause
                     // inefficiencies with things being generated in bulks sometimes?)
+                    this.enemies[j].explode()
                     this.enemies[j].mesh.position.z = farPlane
                     this.enemies[j].mesh.position.x = (Math.random() * 2 - 1) * 1.5
                     this.enemies[j].mesh.position.y = Math.random() * (4 - 1) + 2
@@ -198,7 +262,6 @@ function Enemies(scene, plane) {
         }
 
         this.plane.shots = shotsMissed
-
     }
 }
 
